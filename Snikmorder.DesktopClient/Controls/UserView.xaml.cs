fc40e2443552d219bf36d36c225aa9fc02744e1f
Aksel Kvitberg <aksel.kvitberg@valg.no>
2020-10-03 22:17:02 +0200

    fix #12 - autoscroll

--- Snikmorder.DesktopClient/Controls/UserView.xaml.cs
+++ Snikmorder.DesktopClient/Controls/UserView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using Snikmorder.DesktopClient.GameMock;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Snikmorder.DesktopClient.Controls
 {
@@ -7,6 +9,36 @@ namespace Snikmorder.DesktopClient.Controls
         public UserView()
         {
             InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is TelegramMockUser user)
+            {
+                user.Messages.CollectionChanged += (o, args) =>
+                {
+                    items.ScrollIntoView();
+                };
+            }
         }
     }
+
+    public static class Extensions
+    {
+        public static void ScrollIntoView(this ItemsControl control, object item)
+        {
+            var framework = control.ItemContainerGenerator.ContainerFromItem(item) as FrameworkElement;
+            framework?.BringIntoView();
+        }
+
+        public static void ScrollIntoView(this ItemsControl control)
+        {
+            int count = control.Items.Count;
+            if (count == 0) { return; }
+            object item = control.Items[count - 1];
+            control.ScrollIntoView(item);
+        }
+    }
+
 }
